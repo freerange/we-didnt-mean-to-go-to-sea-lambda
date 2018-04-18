fe0a975ff510fb45d1b6eec0dfa6b784c699e59b
Chris Roos <chris@seagul.co.uk>
2018-04-18 18:08:07 +0100

    Update handler so that it responds to our two Alexa intents

--- handler.js
+++ handler.js
@@ -1,16 +1,22 @@
 'use strict';
+const Alexa = require('alexa-sdk');
+
+const APP_ID = 'amzn1.ask.skill.e12f9983-ae2b-4fd7-a040-26ce6d38024b';
+
+const handlers = {
+  'Listen': function() {
+    this.response.speak('Listen intent');
+    this.emit(':responseReady');
+  },
+  'Move': function() {
+    this.response.speak('Move intent');
+    this.emit(':responseReady');
+  }
+}
 
 module.exports.game = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  const alexa = Alexa.handler(event, context, callback);
+  alexa.APP_ID = APP_ID;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
