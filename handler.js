acc28b03e0c37ef86ba4953e41a74cfb177b578d
Chris Roos <chris@seagul.co.uk>
2018-04-18 18:24:50 +0100

    Start the game on LaunchRequest
    Code copied from https://github.com/freerange/alexa-lambda-gruesome/blob/master/handler.js.

--- handler.js
+++ handler.js
@@ -1,12 +1,33 @@
 'use strict';
 const Alexa = require('alexa-sdk');
+const https = require('https');
 
 const APP_ID = 'amzn1.ask.skill.e12f9983-ae2b-4fd7-a040-26ce6d38024b';
 
 const handlers = {
   'LaunchRequest': function() {
-    this.response.speak('LaunchRequest intent');
-    this.emit(':responseReady');
+    var url = 'https://we-didnt-mean-to-go-to-sea.herokuapp.com/';
+    https.get(url, (resp) => {
+      let data = '';
+
+      // A chunk of data has been recieved.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        var parsedOutput = JSON.parse(data)
+        var text = parsedOutput.ack;
+        this.response.speak(text);
+        this.attributes.state = parsedOutput.state;
+        this.response.shouldEndSession(false);
+        this.emit(':responseReady');
+      });
+
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
   },
   'Listen': function() {
     this.response.speak('Listen intent');
